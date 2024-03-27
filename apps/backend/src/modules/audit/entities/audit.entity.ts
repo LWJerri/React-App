41560e68cdcee017ec3234610276f4202737a6d3
LWJerri <loginmanx@gmail.com>
2024-03-27 11:51:42 +0100

    feat(backend): Add `readonly` property to entity fields

--- apps/backend/src/modules/audit/entities/audit.entity.ts
+++ apps/backend/src/modules/audit/entities/audit.entity.ts
@@ -3,26 +3,26 @@ import { Action, AuditLog } from "@prisma/client";
 
 export class AuditEntity implements AuditLog {
   @ApiProperty({ example: "clu1pdblv000308jxg7xj2bhy", description: "Unique Id of the record in the database." })
-  id: string;
+  readonly id: string;
 
   @ApiProperty({ example: Action.CREATE, enum: Action, description: "What kind of action took place." })
-  action: Action;
+  readonly action: Action;
 
   @ApiProperty({ example: "clu1pe5sc000408jx2fgfdkc8", description: "The unique Id with which the action occurred." })
-  relatedId: string;
+  readonly relatedId: string;
 
   @ApiProperty({ examples: ["name", "description"], description: "What exactly was changed in the record." })
-  affectedField: string;
+  readonly affectedField: string;
 
   @ApiPropertyOptional({ example: "Old Name", description: "The value before the action is performed." })
-  oldState: string | null;
+  readonly oldState: string | null;
 
   @ApiPropertyOptional({ example: "New Name", description: "Value when the action has already been performed." })
-  newState: string | null;
+  readonly newState: string | null;
 
   @ApiProperty({ example: new Date().toISOString(), description: "The date on which the action was performed." })
-  createdAt: Date;
+  readonly createdAt: Date;
 
   @ApiProperty({ example: new Date().toISOString(), description: "The date on which the action was performed." })
-  updatedAt: Date;
+  readonly updatedAt: Date;
 }
--- apps/backend/src/modules/list/entities/get.entity.ts
+++ apps/backend/src/modules/list/entities/get.entity.ts
@@ -3,17 +3,17 @@ import { List } from "@prisma/client";
 
 export class GetEntity implements List {
   @ApiProperty({ example: "clu1pl3ku000508jxh4p5hqrw", description: "Unique Id of the record in the database" })
-  id: string;
+  readonly id: string;
 
   @ApiProperty({ example: "Triage", description: "Title for the list." })
-  name: string;
+  readonly name: string;
 
   @ApiProperty({ example: new Date().toISOString(), description: "Date the list was created." })
-  createdAt: Date;
+  readonly createdAt: Date;
 
   @ApiProperty({ example: new Date().toISOString(), description: "Date the list was updated." })
-  updatedAt: Date;
+  readonly updatedAt: Date;
 
   @ApiProperty({ example: 1, description: "How many tasks does this list have." })
-  task: number;
+  readonly task: number;
 }
