9120c5895f30209329736274d3069ed792af8174
LWJerri <loginmanx@gmail.com>
2024-03-31 00:25:48 +0100

    feat(fronend): Add `tasks` fields to store & Make little changes in `lists` fields

--- apps/frontend/src/app/store.ts
+++ apps/frontend/src/app/store.ts
@@ -1,25 +1,40 @@
 import { Priority } from "@/enums/priority";
 import { Store } from "@/interfaces/Store";
 import { List } from "@/types/List";
+import { Task } from "@/types/Task";
 import { create } from "zustand";
 
 export const store = create<Store>((set, get) => ({
   listId: "",
-  setList: (listId: string) => set(() => ({ listId })),
-  getList: () => get().listId,
+  getListId: () => get().listId,
+  setListId: (listId: string) => set(() => ({ listId })),
 
   dueAt: "",
-  setDueAt: (dueAt: string) => set(() => ({ dueAt })),
   getDueAt: () => get().dueAt,
+  setDueAt: (dueAt: string) => set(() => ({ dueAt })),
 
   priority: Priority.LOW,
-  setPriority: (value: Priority) => set(() => ({ priority: value })),
   getPriority: () => get().priority,
+  setPriority: (value: Priority) => set(() => ({ priority: value })),
 
   lists: [],
-  setLists: (lists: List[]) => set(() => ({ lists })),
-  updateLists: (list: List) => set((state) => ({ lists: [...state.lists, list] })),
   getLists: () => get().lists,
+  getList: (listId: string) => get().lists.find((list) => list.id === listId)!,
+  loadLists: (lists: List[]) => set(() => ({ lists })),
+  addList: (list: Omit<List, "task">) => set(({ lists }) => ({ lists: [{ ...list, task: 0 }, ...lists] })),
+  updateList: (newList: List) => {
+    set(({ lists }) => ({ lists: lists.map((list) => (list.id === newList.id ? newList : list)) }));
+  },
+  removeList: (listId) => set(({ lists }) => ({ lists: lists.filter((list) => list.id !== listId) })),
 
-  reset: () => set({ dueAt: "", listId: "", priority: Priority.LOW }),
+  tasks: [],
+  getTasks: (listId: string) => get().tasks.filter((task) => task.listId === listId),
+  loadTasks: (tasks: Task[]) => set(() => ({ tasks })),
+  addTask: (task: Task) => set(({ tasks }) => ({ tasks: [task, ...tasks] })),
+  updateTask: (newTask: Task) => {
+    set(({ tasks }) => ({ tasks: tasks.map((task) => (task.id === newTask.id ? newTask : task)) }));
+  },
+  removeTask: (taskId) => set(({ tasks }) => ({ tasks: tasks.filter((task) => task.id !== taskId) })),
+
+  reset: () => set({ listId: "", dueAt: "", priority: Priority.LOW }),
 }));
--- apps/frontend/src/interfaces/Store.ts
+++ apps/frontend/src/interfaces/Store.ts
@@ -1,23 +1,34 @@
 import { Priority } from "@/enums/priority";
 import { List } from "@/types/List";
+import { Task } from "@/types/Task";
 
 export interface Store {
   listId: string;
-  setList: (listId: string) => void;
-  getList: () => string;
+  getListId: () => string;
+  setListId: (listId: string) => void;
 
   dueAt: string;
-  setDueAt: (dueAt: string) => void;
   getDueAt: () => string;
+  setDueAt: (dueAt: string) => void;
 
   priority: Priority;
-  setPriority: (priority: Priority) => void;
   getPriority: () => Priority;
+  setPriority: (priority: Priority) => void;
 
   lists: List[];
-  setLists: (lists: List[]) => void;
-  updateLists: (list: List) => void;
   getLists: () => List[];
+  getList: (listId: string) => List;
+  loadLists: (lists: List[]) => void;
+  addList: (list: Omit<List, "task">) => void;
+  updateList: (list: List) => void;
+  removeList: (listId: string) => void;
+
+  tasks: Task[];
+  getTasks: (listId: string) => Task[];
+  loadTasks: (tasks: Task[]) => void;
+  addTask: (task: Task) => void;
+  updateTask: (task: Task) => void;
+  removeTask: (taskId: string) => void;
 
   reset: () => void;
 }
