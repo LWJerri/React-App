84d0c4b145ed901860254db81e83fc594bfad2c4
LWJerri <loginmanx@gmail.com>
2024-03-30 13:38:18 +0100

    feat(backend): Remove `priority` module

--- apps/backend/src/modules/app/app.module.ts
+++ apps/backend/src/modules/app/app.module.ts
@@ -3,17 +3,9 @@ import { ConfigModule } from "@nestjs/config";
 import { LoggerModule } from "nestjs-pino";
 import { AuditModule } from "../audit/audit.module";
 import { ListModule } from "../list/list.module";
-import { PriorityModule } from "../priority/priority.module";
 import { TaskModule } from "../task/task.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    LoggerModule.forRoot(),
-    ListModule,
-    TaskModule,
-    AuditModule,
-    PriorityModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), LoggerModule.forRoot(), ListModule, TaskModule, AuditModule],
 })
 export class AppModule {}
--- apps/backend/src/modules/priority/priority.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { PriorityController } from "./priority.controller";
-import { PriorityService } from "./priority.service";
-
-@Module({
-  controllers: [PriorityController],
-  providers: [PriorityService],
-})
-export class PriorityModule {}
