4ae47f9773b4d41f5a6fb14314825d2ebadf1661
LWJerri <loginmanx@gmail.com>
2024-03-30 20:38:28 +0100

    feat(frontend): Create `KanbanDropdown` component

---
+++ apps/frontend/src/components/custom/kanban/KanbanDropdown.tsx
@@ -0,0 +1,92 @@
+import api from "@/app/api";
+import { store } from "@/app/store";
+import { useToast } from "@/components/ui/use-toast";
+import { List } from "@/types/List";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { IconDotsVertical, IconEdit, IconPlus, IconTrash } from "@tabler/icons-react";
+import { useState } from "react";
+import { Button } from "../../ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem } from "../../ui/dropdown-menu";
+import EditList from "../list/EditList";
+import NewTask from "../task/NewTask";
+
+const KanbanDropdown = (props: { list: List }) => {
+  const { list } = props;
+
+  const { toast } = useToast();
+
+  const [openEditList, setOpenEditList] = useState(false);
+  const [openNewTask, setOpenNewTask] = useState(false);
+
+  const removeList = store((state) => state.removeList);
+  const setListId = store((state) => state.setListId);
+
+  async function deleteList(listId: string) {
+    const { error, data } = await api.DELETE("/api/lists/{id}", { params: { path: { id: listId } } });
+
+    if (data) {
+      toast({ title: "List deleted", description: "List successfully deleted." });
+
+      removeList(listId);
+
+      return;
+    }
+
+    if (!error) {
+      toast({ title: "Something went wrong", description: "Please try again later.", variant: "destructive" });
+
+      return;
+    }
+
+    toast({
+      title: error.error ?? "Something went wrong",
+      description: typeof error.message === "string" ? error.message : error.message.join(", "),
+      variant: "destructive",
+    });
+  }
+
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size={"icon"} variant={"ghost"}>
+            <IconDotsVertical stroke={1.5} />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuGroup>
+            <DropdownMenuItem
+              onClick={() => {
+                setListId(list.id);
+                setOpenEditList(!openEditList);
+              }}
+            >
+              <IconEdit stroke={1.5} size={16} className="mr-2" />
+              <span>Edit</span>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem
+              onClick={() => {
+                setListId(list.id);
+                setOpenNewTask(!openNewTask);
+              }}
+            >
+              <IconPlus stroke={1.5} size={16} className="mr-2" />
+              <span>Add new task</span>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem className="text-[#ED4245]" onClick={() => deleteList(list.id)}>
+              <IconTrash stroke={1.5} size={16} className="mr-2" />
+              <span>Delete</span>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <EditList open={openEditList} close={() => setOpenEditList(false)} listId={list.id} />
+      <NewTask open={openNewTask} close={() => setOpenNewTask(false)} listId={list.id} />
+    </div>
+  );
+};
+
+export default KanbanDropdown;
