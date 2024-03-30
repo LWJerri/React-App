cd1f28f7daecdc500510d79ec1ba653bbc96dcb6
LWJerri <loginmanx@gmail.com>
2024-03-30 17:02:27 +0100

    feat(frontend): Use `Toaster` component to display all notifications

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { Toaster } from "./components/ui/toaster";
 import "./index.css";
 
 const container = document.getElementById("root");
@@ -12,5 +13,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
+
+    <Toaster />
   </React.StrictMode>,
 );
