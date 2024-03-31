921e7dad939e00ea8471362278e2ee0bc75e862c
LWJerri <loginmanx@gmail.com>
2024-03-31 15:05:39 +0200

    feat(frontend): Rewrite store

--- apps/frontend/src/app/store.ts
+++ apps/frontend/src/app/store.ts
@@ -1,52 +1,42 @@
-import { Priority } from "@/enums/priority";
 import { Store } from "@/interfaces/Store";
 import { List } from "@/types/List";
 import { Task } from "@/types/Task";
 import { create } from "zustand";
 
 export const store = create<Store>((set, get) => ({
-  listId: "",
-  getListId: () => get().listId,
-  setListId: (listId: string) => set(() => ({ listId })),
-
-  priority: Priority.LOW,
-  getPriority: () => get().priority,
-  setPriority: (value: Priority) => set(() => ({ priority: value })),
-
   lists: [],
   getLists: () => get().lists,
   getList: (listId: string) => get().lists.find((list) => list.id === listId)!,
-  loadLists: (lists: List[]) => set(() => ({ lists })),
-  addList: (list: Omit<List, "task">) => set(({ lists }) => ({ lists: [{ ...list, task: 0 }, ...lists] })),
-  updateList: (newList: List) => {
-    set(({ lists }) => ({ lists: lists.map((list) => (list.id === newList.id ? newList : list)) }));
+  addLists: (newLists: List[]) => set(({ lists }) => ({ lists: [...newLists, ...lists] })),
+  updateList: (updatedList: List) => {
+    set(({ lists }) => ({ lists: lists.map((list) => (list.id === updatedList.id ? updatedList : list)) }));
   },
-  removeList: (listId) => set(({ lists }) => ({ lists: lists.filter((list) => list.id !== listId) })),
+  removeList: (listId: string) => set(({ lists }) => ({ lists: lists.filter((list) => list.id !== listId) })),
 
   tasks: [],
   getTasks: (listId: string) => get().tasks.filter((task) => task.listId === listId),
-  loadTasks: (newTasks: Task[]) =>
+  getTask: (taskId: string) => get().tasks.find((task) => task.id === taskId)!,
+  addTasks: (newTasks: Task[]) => {
     set(({ tasks }) => ({
-      tasks: tasks.filter((st) => !newTasks.map((nt) => nt.id).includes(st.id)).concat(newTasks),
-    })),
-  addTask: (task: Task) => {
-    set(({ tasks }) => ({ tasks: [task, ...tasks] }));
+      tasks: tasks
+        .filter((storeTask) => !newTasks.map((newTask) => newTask.id).includes(storeTask.id))
+        .concat(newTasks),
+    }));
     set(({ lists }) => ({
       lists: lists.map((list) => ({ ...list, task: get().getTasks(list.id).length })),
     }));
   },
-  updateTask: (newTask: Task) => {
+  updateTask: (updatedTask: Task) => {
     set(({ tasks }) => ({
       tasks: tasks
-        .map((task) => (task.id === newTask.id ? newTask : task))
-        .sort((a, b) => {
-          const timeA = new Date(a.createdAt).getTime();
-          const timeB = new Date(b.createdAt).getTime();
+        .map((task) => (task.id === updatedTask.id ? updatedTask : task))
+        .sort((start, end) => {
+          const timeA = new Date(start.createdAt).getTime();
+          const timeB = new Date(end.createdAt).getTime();
 
           return timeB - timeA;
         }),
     }));
-
     set(({ lists }) => ({
       lists: lists.map((list) => ({ ...list, task: get().getTasks(list.id).length })),
     }));
@@ -57,6 +47,4 @@ export const store = create<Store>((set, get) => ({
       lists: lists.map((list) => ({ ...list, task: get().getTasks(list.id).length })),
     }));
   },
-
-  reset: () => set({ listId: "", priority: Priority.LOW }),
 }));
--- apps/frontend/src/interfaces/Store.ts
+++ apps/frontend/src/interfaces/Store.ts
@@ -1,30 +1,18 @@
-import { Priority } from "@/enums/priority";
 import { List } from "@/types/List";
 import { Task } from "@/types/Task";
 
 export interface Store {
-  listId: string;
-  getListId: () => string;
-  setListId: (listId: string) => void;
-
-  priority: Priority;
-  getPriority: () => Priority;
-  setPriority: (priority: Priority) => void;
-
   lists: List[];
   getLists: () => List[];
   getList: (listId: string) => List;
-  loadLists: (lists: List[]) => void;
-  addList: (list: Omit<List, "task">) => void;
+  addLists: (lists: List[]) => void;
   updateList: (list: List) => void;
   removeList: (listId: string) => void;
 
   tasks: Task[];
   getTasks: (listId: string) => Task[];
-  loadTasks: (tasks: Task[]) => void;
-  addTask: (task: Task) => void;
+  getTask: (taskId: string) => Task;
+  addTasks: (tasks: Task[]) => void;
   updateTask: (task: Task) => void;
   removeTask: (taskId: string) => void;
-
-  reset: () => void;
 }
