8103cb41418790db4d85b5a97bcfb589f6c56680
LWJerri <loginmanx@gmail.com>
2024-03-31 05:02:24 +0200

    feat(frontend): Make UX better

--- apps/frontend/src/components/custom/kanban/KanbanTask.tsx
+++ apps/frontend/src/components/custom/kanban/KanbanTask.tsx
@@ -5,7 +5,6 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/componen
 import { Label } from "@/components/ui/label";
 import { useToast } from "@/components/ui/use-toast";
 import { cn } from "@/lib/utils";
-import { Task } from "@/types/Task";
 import { IconCalendarDue } from "@tabler/icons-react";
 import { format } from "date-fns";
 import { useEffect, useState } from "react";
@@ -13,77 +12,92 @@ import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import ListSelector from "../selectors/ListSelector";
 import TaskDropdown from "../task/TaskDropdown";
+import TaskSkeleton from "../task/TaskSkeleton";
 
 const KanbanTask = (props: { listId: string }) => {
   const { listId } = props;
 
   const { toast } = useToast();
 
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  store.subscribe(({ tasks }) => setTasks(tasks));
+  const tasks = store((state) => state.getTasks(listId));
+  const { task: tasksCount } = store((state) => state.getList(listId));
 
   const loadTasksToStore = store((state) => state.loadTasks);
 
   useEffect(() => {
-    api.GET("/api/lists/{listId}/tasks", { params: { path: { listId } } }).then(({ data, error }) => {
-      if (data) return loadTasksToStore(data);
+    api
+      .GET("/api/lists/{listId}/tasks", { params: { path: { listId } } })
+      .then(({ data, error }) => {
+        if (data) return loadTasksToStore(data);
 
-      if (!error) {
-        toast({ title: "Something went wrong", description: "Please try again later.", variant: "destructive" });
+        if (!error) {
+          toast({ title: "Something went wrong", description: "Please try again later.", variant: "destructive" });
 
-        return;
-      }
+          return;
+        }
 
-      toast({
-        title: error.error ?? "Something went wrong",
-        description: typeof error.message === "string" ? error.message : error.message.join(", "),
-        variant: "destructive",
-      });
-    });
+        toast({
+          title: error.error ?? "Something went wrong",
+          description: typeof error.message === "string" ? error.message : error.message.join(", "),
+          variant: "destructive",
+        });
+      })
+      .finally(() => setLoading(false));
   }, []);
 
   return (
     <div className="grid gap-4">
-      {tasks.map((task) => (
-        <Card
-          className={cn(
-            "transition-all duration-150 hover:cursor-pointer hover:border-dashed",
-            task.priority === "CRITICAL" || task.priority === "HIGH" ? "hover:bg-red-300" : "hover:bg-muted ",
-          )}
-          key={task.id}
-        >
-          <CardHeader>
-            <div className="flex w-full items-center justify-between">
-              <CardTitle>{task.name}</CardTitle>
-              <TaskDropdown task={task} />
-            </div>
-          </CardHeader>
+      {loading &&
+        [...Array(tasksCount > 10 ? 10 : tasksCount)].map((_, i) => (
+          <div key={i}>
+            <TaskSkeleton />
+          </div>
+        ))}
 
-          <CardContent className="small max-h-96 font-normal">
-            <Markdown remarkPlugins={[remarkGfm]}>
-              {task.description.length > 500 ? task.description.slice(0, 500) + "..." : task.description}
-            </Markdown>
-          </CardContent>
+      {!loading &&
+        tasks.map((task) => (
+          <Card
+            className={cn(
+              "transition-all duration-150 hover:cursor-pointer hover:border-dashed",
+              task.priority === "CRITICAL" || task.priority === "HIGH" ? "hover:bg-red-300" : "hover:bg-muted ",
+            )}
+            key={task.id}
+          >
+            <CardHeader>
+              <div className="flex w-full items-center justify-between">
+                <CardTitle>{task.name}</CardTitle>
+                <TaskDropdown task={task} />
+              </div>
+            </CardHeader>
 
-          <CardFooter>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-row items-center justify-between">
-                <div className="flex items-center space-x-1.5">
-                  <IconCalendarDue stroke={1.5} />
-                  <Label htmlFor="name">{format(task.dueAt, "dd.MM.yyyy")}</Label>
+            <CardContent className="small max-h-96 font-normal">
+              <Markdown remarkPlugins={[remarkGfm]}>
+                {task.description.length > 500 ? task.description.slice(0, 500) + "..." : task.description}
+              </Markdown>
+            </CardContent>
+
+            <CardFooter>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-row items-center justify-between">
+                  <div className="flex items-center space-x-1.5">
+                    <IconCalendarDue stroke={1.5} />
+                    <Label htmlFor="name">{format(task.dueAt, "dd.MM.yyyy")}</Label>
+                  </div>
+
+                  <Badge
+                    variant={task.priority === "CRITICAL" || task.priority === "HIGH" ? "destructive" : "secondary"}
+                  >
+                    {task.priority}
+                  </Badge>
                 </div>
 
-                <Badge variant={task.priority === "CRITICAL" || task.priority === "HIGH" ? "destructive" : "secondary"}>
-                  {task.priority}
-                </Badge>
+                <ListSelector placeholder="Move to..." />
               </div>
-
-              <ListSelector placeholder="Move to..." />
-            </div>
-          </CardFooter>
-        </Card>
-      ))}
+            </CardFooter>
+          </Card>
+        ))}
     </div>
   );
 };
