d0efee1f4b3140529e0b65cc4334727e96df4292
LWJerri <loginmanx@gmail.com>
2024-04-01 20:03:58 +0200

    feat(frontend): Remove `VITE_API_URL`

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { "@": resolve(__dirname, "./src") } },
-  server: { proxy: { "/api": { target: process.env.VITE_API_URL ?? "http://localhost:3000" } } },
+  server: { proxy: { "/api": { target: "http://localhost:3000" } } },
 });
